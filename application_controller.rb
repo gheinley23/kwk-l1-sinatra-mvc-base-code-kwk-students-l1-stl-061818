3eed6bae651db9da4a7657705d983cefc5a41eb0
gheinley23 <gheinley23@jacksonindians.us>
2018-06-30 00:33:16 +0000

    automatically backed up by learn

--- application_controller.rb
+++ application_controller.rb
@@ -5,6 +5,12 @@ class MyApp < Sinatra::Base
 
   get '/' do
     erb :index
+  images = [ 'https://media3.giphy.com/media/1qYI0zJGlCzG1IOnsx/giphy.gif', 'https://media1.giphy.com/media/k5VWhypb68TeeIP2Cv/giphy.gif' ]
+# execute image_path helper method on each element of images
+@paths = images.map {|name| image_path name}
+
   end
+
+
 
 end
